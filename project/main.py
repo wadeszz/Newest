87d3fe15c27a31b1d87edc97a5113a8a957ebae7
wadeszz <victorsemenov97@gmail.com>
2025-07-18 08:08:53 +0300

    Delete project/main.py

--- project/main.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class TravelSimulatorApp(App):
-    def build(self):
-        return Label(text="Добро пожаловать в Travel Simulator!")
-
-if __name__ == "__main__":
-    TravelSimulatorApp().run()
